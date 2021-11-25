93ae6bbbff91e37b41c08b54077fd3d6f5a7aa9b
0zz10_mac <zhou.zhenj@northeastern.edu>
2021-11-24 23:32:14 -0800

    draft webservice skeleton

---
+++ app/src/main/java/com/numad21fa/vital/webservice/FDCWebServiceActivity.java
@@ -0,0 +1,17 @@
+package com.numad21fa.vital.webservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// https://developer.android.com/training/volley/simple
+// https://www.youtube.com/watch?v=8UgFclFBPcM
+
+public class FDCWebServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fdcweb_service);
+    }
+}
