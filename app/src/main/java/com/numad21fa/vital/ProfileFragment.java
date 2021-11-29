2c446eec70fd3e964a66b7d9a4462faccce62857
jsyllu <yliu.jes@gmail.com>
2021-11-28 21:06:34 -0500

    created bottom navigation fragments

---
+++ app/src/main/java/com/numad21fa/vital/ProfileFragment.java
@@ -0,0 +1,84 @@
+package com.numad21fa.vital;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass. Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment implements OnClickListener {
+
+  // TODO: Rename parameter arguments, choose names that match
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "param1";
+  private static final String ARG_PARAM2 = "param2";
+
+  // TODO: Rename and change types of parameters
+  private String mParam1;
+  private String mParam2;
+
+  public ProfileFragment() {
+    // Required empty public constructor
+  }
+
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided
+   * parameters.
+   *
+   * @param param1 Parameter 1.
+   * @param param2 Parameter 2.
+   * @return A new instance of fragment ProfileFragment.
+   */
+  // TODO: Rename and change types and number of parameters
+  public static ProfileFragment newInstance(String param1, String param2) {
+    ProfileFragment fragment = new ProfileFragment();
+    Bundle args = new Bundle();
+    args.putString(ARG_PARAM1, param1);
+    args.putString(ARG_PARAM2, param2);
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      mParam1 = getArguments().getString(ARG_PARAM1);
+      mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View view = inflater.inflate(R.layout.fragment_profile, container, false);;
+    Button btn_login = view.findViewById(R.id.btn_login);
+    btn_login.setOnClickListener(this);
+
+    return view;
+  }
+
+  public void login(View view) {
+    // TODO: complete authentication logic
+    // open login screen
+    Intent intent = new Intent(getActivity(), LoginActivity.class);
+    startActivity(intent);
+  }
+
+  @Override
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.btn_login:
+        login(this.getView());
+        break;
+    }
+  }
+}
