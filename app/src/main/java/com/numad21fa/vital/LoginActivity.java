0af5f484441b9a2a8e853f9e724caa0590c0ee60
Lori <57802869+loriZ77@users.noreply.github.com>
2021-12-10 00:26:58 -0800

    user can log in

--- app/src/main/java/com/numad21fa/vital/LoginActivity.java
+++ app/src/main/java/com/numad21fa/vital/LoginActivity.java
@@ -2,12 +2,18 @@ package com.numad21fa.vital;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,7 +31,6 @@ public class LoginActivity extends AppCompatActivity {
     fAuth = FirebaseAuth.getInstance();
     Button btn_exit = findViewById(R.id.btn_exit);
     Button btn_toRegisterPage = findViewById(R.id.btn_toRegPage);
-    //btn_toRegisterPage.setOnClickListener();
     btn_exit.setOnClickListener(v -> finish());
   }
 
@@ -34,10 +39,39 @@ public class LoginActivity extends AppCompatActivity {
     startActivity(intent);
   }
 
+  public void login() {
+    String email = userEmail.getText().toString().trim();
+    String password = userPassword.getText().toString().trim();
+    if (TextUtils.isEmpty(email)) {
+      userEmail.setError("Email is required!");
+      return;
+    }
+    if (TextUtils.isEmpty(password)) {
+      userPassword.setError("Password is required!");
+      return;
+    }
+    fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+      @Override
+      public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+          Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+          startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        } else {
+          Toast.makeText(LoginActivity.this, "ERROR: " + task.getException() , Toast.LENGTH_SHORT).show();
+
+        }
+      }
+    });
+  }
+
   public void onClick(View view) {
     switch (view.getId()) {
       case R.id.btn_toRegPage:
         toRegPage(view);
+        break;
+
+      case R.id.btn_logIn:
+        login();
         break;
     }
   }
